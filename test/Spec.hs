3b225fbb9dd8427f6696eff8c3317fe3377dae2c
Nikita Tchayka <nikitatchayka@gmail.com>
2018-11-30 17:41:14 -0800

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,17 @@
-import Relude
+import           Relude                  hiding ( head )
+import           Relude.Unsafe
+
+import           Test.Hspec
+import           Test.QuickCheck
+
+
 main :: IO ()
-main = putTextLn "Test suite not yet implemented"
+main = hspec $ describe "Prelude.head" $ do
+  it "returns the first element of a list" $ head [23 ..] `shouldBe` (23 :: Int)
+
+  it "returns the first element of an *arbitrary* list" $ property $ \x xs ->
+    head (x : xs) == (x :: Int)
+
+  it "throws an exception if used with an empty list"
+    $             evaluateWHNF (head [])
+    `shouldThrow` anyException
