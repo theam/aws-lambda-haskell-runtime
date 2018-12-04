db94e8331c4a8b37133a8fa5baebf5f25978a9d7
Nikita Tchayka <nikitatchayka@gmail.com>
2018-12-04 21:12:47 +0000

    Begin code generation

--- src/Aws/Lambda/Function.hs
+++ src/Aws/Lambda/Function.hs
@@ -3,11 +3,18 @@ module Aws.Lambda.Function where
 import Relude
 
 import qualified Options.Generic as Options
-
-import AWS.Lambda.Runtime
+import Language.Haskell.TH
 
 data LambdaOptions = LambdaOptions
-  { eventObject :: Text
-  , contextObject :: Context
+  { eventObject     :: Text
+  , contextObject   :: Text
+  , functionHandler :: Text
   } deriving (Generic)
 instance Options.ParseRecord LambdaOptions
+
+
+mkMain :: Q [Dec]
+mkMain = do
+  let mainName = mkName "main"
+  body <- [e|putStrLn "hi, im generated"|]
+  return [ FunD mainName [Clause [] (NormalB body) [] ]]
