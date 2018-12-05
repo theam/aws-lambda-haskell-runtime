ab2cb2a4fa0ff0c52e8cbffe63b8fca4e8d9fe46
Nikita Tchayka <nikitatchayka@gmail.com>
2018-12-05 00:20:04 +0000

    Export needed functions for metaprogramming

--- src/Aws/Lambda/Function.hs
+++ src/Aws/Lambda/Function.hs
@@ -1,6 +1,14 @@
-module Aws.Lambda.Function where
+module Aws.Lambda.Function
+  ( LambdaOptions (..)
+  , mkMain
+  , returnAndFail
+  , returnAndSucceed
+  , Options.getRecord
+  )
+where
 
 import Relude
+import Data.Aeson
 
 import qualified Options.Generic as Options
 import Language.Haskell.TH
@@ -18,3 +26,14 @@ mkMain = do
   let mainName = mkName "main"
   body <- [e|putStrLn ("hi, im generated" :: String)|]
   return [ FunD mainName [Clause [] (NormalB body) [] ]]
+
+
+returnAndFail :: ToJSON a => a -> IO ()
+returnAndFail v = do
+ putTextLn (decodeUtf8 $ encode v)
+ exitFailure
+
+returnAndSucceed :: ToJSON a => a -> IO ()
+returnAndSucceed v = do
+ putTextLn (decodeUtf8 $  encode v)
+ exitFailure
