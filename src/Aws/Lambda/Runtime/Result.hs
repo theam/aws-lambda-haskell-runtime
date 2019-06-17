f2ae0f3693ac4cf2d7ecd491c98c7f5b2dbd3e9a
Nikita Tchayka <nikitatchayka@gmail.com>
2019-06-17 11:33:56 +0100

    Remove ToJSON instance

--- src/Aws/Lambda/Runtime/Result.hs
+++ src/Aws/Lambda/Runtime/Result.hs
@@ -2,11 +2,5 @@ module Aws.Lambda.Runtime.Result
   ( LambdaResult(..)
   ) where
 
-import Data.Aeson
-
 newtype LambdaResult =
   LambdaResult String
-
-instance ToJSON LambdaResult where
-  toJSON (LambdaResult result) =
-    toJSON result
