73529ade056586406323f1cdf76cdfd0852c8115
Nikita Tchayka <nick@booster.cloud>
2020-07-03 16:33:19 +0100

    WIP

--- src/Aws/Lambda.hs
+++ src/Aws/Lambda.hs
@@ -1,8 +1,8 @@
 module Aws.Lambda
-  ( module Reexported
-  ) where
+  ( module Reexported,
+  )
+where
 
-import Aws.Lambda.Configuration as Reexported
 import Aws.Lambda.Runtime as Reexported
-import Aws.Lambda.Runtime.Context as Reexported
 import Aws.Lambda.Runtime.ApiGatewayInfo as Reexported
+import Aws.Lambda.Runtime.Context as Reexported
---
+++ weeder.dhall
@@ -0,0 +1,7 @@
+{ roots =
+  [ "^Aws.Lambda.Runtime.runLambda$"
+  , "^Main.spec$"
+  , "^Main.main$"
+  ]
+, type-class-roots = True
+}
