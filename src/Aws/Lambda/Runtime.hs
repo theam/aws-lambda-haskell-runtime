e32f695dd60e90022abb0c2ba7b9c2152ac502dc
Nikita Tchayka <nick@booster.cloud>
2020-07-03 14:42:05 +0100

    Initial version with no TH

--- src/Aws/Lambda/Runtime.hs
+++ src/Aws/Lambda/Runtime.hs
@@ -1,26 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
-
 module Aws.Lambda.Runtime
-  ( runLambda
-  , Runtime.LambdaResult(..)
-  , Runtime.DispatcherStrategy(..)
-  , Runtime.DispatcherOptions(..)
-  , Runtime.ApiGatewayDispatcherOptions(..)
-  , Runtime.defaultDispatcherOptions
-  , Error.Parsing(..)
-  ) where
-
-import Control.Exception.Safe.Checked
-import qualified Control.Exception.Safe.Checked as Checked
-import Control.Monad (forever)
-import System.IO (hFlush, stdout, stderr)
-import qualified Network.HTTP.Client as Http
-
-import Data.Aeson
-import Data.IORef
+  ( runLambda,
+    Runtime.LambdaResult (..),
+    Runtime.DispatcherStrategy (..),
+    Runtime.DispatcherOptions (..),
+    Runtime.ApiGatewayDispatcherOptions (..),
+    Runtime.defaultDispatcherOptions,
+    Error.Parsing (..),
+  )
+where
 
 import qualified Aws.Lambda.Runtime.ApiInfo as ApiInfo
 import qualified Aws.Lambda.Runtime.Common as Runtime
@@ -28,84 +19,102 @@ import qualified Aws.Lambda.Runtime.Context as Context
 import qualified Aws.Lambda.Runtime.Environment as Environment
 import qualified Aws.Lambda.Runtime.Error as Error
 import qualified Aws.Lambda.Runtime.Publish as Publish
+import qualified Aws.Lambda.Utilities as Utilities
 import qualified Control.Exception as Unchecked
+import Control.Exception.Safe.Checked
+import qualified Control.Exception.Safe.Checked as Checked
+import Control.Monad (forever)
+import Data.Aeson
+import Data.IORef
+import qualified Network.HTTP.Client as Http
+import System.IO (hFlush, stderr, stdout)
 
 -- | Runs the user @haskell_lambda@ executable and posts back the
 -- results. This is called from the layer's @main@ function.
-runLambda :: forall context. IO context -> Runtime.RunCallback context -> IO ()
-runLambda initializeCustomContext callback = do
+runLambda ::
+  forall context event error result.
+  FromJSON event =>
+  ToJSON error =>
+  ToJSON result =>
+  IO context ->
+  (Context.Context context -> event -> IO (Either error result)) ->
+  IO ()
+runLambda initializeCustomContext userCode = do
   manager <- Http.newManager httpManagerSettings
   customContext <- initializeCustomContext
   customContextRef <- newIORef customContext
-  context   <- Context.initialize @context customContextRef `catch` errorParsing `catch` variableNotSet
+  context <- Context.initialize @context customContextRef `catch` errorParsing `catch` variableNotSet
   forever $ do
     lambdaApi <- Environment.apiEndpoint `catch` variableNotSet
-    event     <- ApiInfo.fetchEvent manager lambdaApi `catch` errorParsing
-
+    event <- ApiInfo.fetchEvent manager lambdaApi `catch` errorParsing
     -- Purposefully shadowing to prevent using the initial "empty" context
     context <- Context.setEventData context event
-
-    (((invokeAndRun callback manager lambdaApi event context
-      `Checked.catch` \err -> Publish.parsingError err lambdaApi context manager)
-      `Checked.catch` \err -> Publish.invocationError err lambdaApi context manager)
-      `Checked.catch` \(err :: Error.EnvironmentVariableNotSet) -> Publish.runtimeInitError err lambdaApi context manager)
+    ( ( ( invokeAndRun userCode manager lambdaApi event context
+            `Checked.catch` \err -> Publish.parsingError err lambdaApi context manager
+        )
+          `Checked.catch` \err -> Publish.invocationError err lambdaApi context manager
+      )
+        `Checked.catch` \(err :: Error.EnvironmentVariableNotSet) -> Publish.runtimeInitError err lambdaApi context manager
+      )
       `Unchecked.catch` \err -> Publish.invocationError err lambdaApi context manager
 
 httpManagerSettings :: Http.ManagerSettings
 httpManagerSettings =
   -- We set the timeout to none, as AWS Lambda freezes the containers.
   Http.defaultManagerSettings
-  { Http.managerResponseTimeout = Http.responseTimeoutNone
-  }
+    { Http.managerResponseTimeout = Http.responseTimeoutNone
+    }
 
-invokeAndRun
-  :: Throws Error.Invocation
-  => Throws Error.EnvironmentVariableNotSet
-  => Runtime.RunCallback context
-  -> Http.Manager
-  -> String
-  -> ApiInfo.Event
-  -> Context.Context context
-  -> IO ()
-invokeAndRun callback manager lambdaApi event context = do
-  result <- invokeWithCallback callback event context
-
+invokeAndRun ::
+  Throws Error.Invocation =>
+  FromJSON event =>
+  ToJSON error =>
+  ToJSON result =>
+  (Context.Context context -> event -> IO (Either error result)) ->
+  Http.Manager ->
+  String ->
+  ApiInfo.Event ->
+  Context.Context context ->
+  IO ()
+invokeAndRun userCode manager lambdaApi event context = do
+  result <- invokeWithCallback userCode event context
   Publish.result result lambdaApi context manager
     `catch` \err -> Publish.invocationError err lambdaApi context manager
 
-invokeWithCallback
-  :: Throws Error.Invocation
-  => Throws Error.EnvironmentVariableNotSet
-  => Runtime.RunCallback context
-  -> ApiInfo.Event
-  -> Context.Context context
-  -> IO Runtime.LambdaResult
-invokeWithCallback callback event context = do
-  handlerName <- Environment.handlerName
-  let lambdaOptions = Runtime.LambdaOptions
-                      { eventObject = ApiInfo.event event
-                      , functionHandler = handlerName
-                      , executionUuid = ""  -- DirectCall doesnt use UUID
-                      , contextObject = context
-                      }
-  result <- callback lambdaOptions
+invokeWithCallback ::
+  Throws Error.Invocation =>
+  FromJSON event =>
+  ToJSON error =>
+  ToJSON result =>
+  (Context.Context context -> event -> IO (Either error result)) ->
+  ApiInfo.Event ->
+  Context.Context context ->
+  IO Runtime.LambdaResult
+invokeWithCallback userCode event context = do
+  eventObject <-
+    case eitherDecode $ ApiInfo.event event of
+      Right obj -> pure obj
+      Left err -> throw $ Error.Invocation $ toJSON err
+  result <- userCode context eventObject
   -- Flush output to insure output goes into CloudWatch logs
   flushOutput
   case result of
-    Left lambdaError -> case lambdaError of
-      Runtime.StandaloneLambdaError err ->
-        throw $ Error.Invocation $ toJSON err
-      Runtime.ApiGatewayLambdaError err ->
-        throw $ Error.Invocation $ toJSON err
+    Left err ->
+      throw $ Error.Invocation $ toJSON err
+    -- Left lambdaError -> case lambdaError of
+    --   Runtime.StandaloneLambdaError err ->
+    --     throw $ Error.Invocation $ toJSON err
+    --   Runtime.ApiGatewayLambdaError err ->
+    --     throw $ Error.Invocation $ toJSON err
     Right value ->
-      pure value
+      pure (Runtime.StandaloneLambdaResult (Runtime.LambdaResponseBody $ Utilities.toJSONText value))
 
 variableNotSet :: Error.EnvironmentVariableNotSet -> IO a
 variableNotSet (Error.EnvironmentVariableNotSet env) =
   error ("Error initializing, variable not set: " <> env)
 
 errorParsing :: Error.Parsing -> IO a
-errorParsing Error.Parsing{..} =
+errorParsing Error.Parsing {..} =
   error ("Failed parsing " <> errorMessage <> ", got" <> actualValue)
 
 -- | Flush standard output ('stdout') and standard error output ('stderr') handlers
--- src/Aws/Lambda/Runtime/Common.hs
+++ src/Aws/Lambda/Runtime/Common.hs
@@ -1,21 +1,22 @@
-{-# LANGUAGE DeriveLift                 #-}
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE DeriveLift #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Aws.Lambda.Runtime.Common
-  ( RunCallback
-  , LambdaResult(..)
-  , LambdaError(..)
-  , LambdaOptions(..)
-  , DispatcherOptions(..)
-  , ApiGatewayDispatcherOptions(..)
-  , DispatcherStrategy(..)
-  , ToLambdaResponseBody(..)
-  , unLambdaResponseBody
-  , defaultDispatcherOptions
-  ) where
+  ( RunCallback,
+    LambdaResult (..),
+    LambdaError (..),
+    LambdaOptions (..),
+    DispatcherOptions (..),
+    ApiGatewayDispatcherOptions (..),
+    DispatcherStrategy (..),
+    ToLambdaResponseBody (..),
+    LambdaResponseBody (..),
+    defaultDispatcherOptions,
+  )
+where
 
 import Aws.Lambda.Runtime.ApiGatewayInfo
 import Aws.Lambda.Runtime.Context (Context)
@@ -28,24 +29,28 @@ import GHC.Generics (Generic)
 import Language.Haskell.TH.Syntax (Lift)
 
 -- | API Gateway specific dispatcher options
-newtype ApiGatewayDispatcherOptions = ApiGatewayDispatcherOptions
-  { propagateImpureExceptions :: Bool
-  -- ^ Should impure exceptions be propagated through the API Gateway interface
-  } deriving (Lift)
+newtype ApiGatewayDispatcherOptions
+  = ApiGatewayDispatcherOptions
+      { -- | Should impure exceptions be propagated through the API Gateway interface
+        propagateImpureExceptions :: Bool
+      }
+  deriving (Lift)
 
 -- | Options that the dispatcher generator expects
-newtype DispatcherOptions = DispatcherOptions
-  { apiGatewayDispatcherOptions :: ApiGatewayDispatcherOptions
-  } deriving (Lift)
+newtype DispatcherOptions
+  = DispatcherOptions
+      { apiGatewayDispatcherOptions :: ApiGatewayDispatcherOptions
+      }
+  deriving (Lift)
 
 defaultDispatcherOptions :: DispatcherOptions
 defaultDispatcherOptions =
   DispatcherOptions (ApiGatewayDispatcherOptions True)
 
 -- | A strategy on how to generate the dispatcher functions
-data DispatcherStrategy =
-  UseWithAPIGateway |
-  StandaloneLambda
+data DispatcherStrategy
+  = UseWithAPIGateway
+  | StandaloneLambda
   deriving (Lift)
 
 -- | Callback that we pass to the dispatcher function
@@ -53,8 +58,8 @@ type RunCallback context =
   LambdaOptions context -> IO (Either LambdaError LambdaResult)
 
 -- | Wrapper type for lambda response body
-newtype LambdaResponseBody =
-  LambdaResponseBody { unLambdaResponseBody :: Text }
+newtype LambdaResponseBody
+  = LambdaResponseBody {unLambdaResponseBody :: Text}
   deriving newtype (ToJSON, FromJSON)
 
 class ToLambdaResponseBody a where
@@ -72,19 +77,21 @@ instance ToJSON a => ToLambdaResponseBody a where
   toStandaloneLambdaResponse = LambdaResponseBody . toJSONText
 
 -- | Wrapper type for lambda execution results
-data LambdaError =
-  StandaloneLambdaError LambdaResponseBody |
-  ApiGatewayLambdaError (ApiGatewayResponse ApiGatewayResponseBody)
+data LambdaError
+  = StandaloneLambdaError LambdaResponseBody
+  | ApiGatewayLambdaError (ApiGatewayResponse ApiGatewayResponseBody)
 
 -- | Wrapper type to handle the result of the user
-data LambdaResult =
-  StandaloneLambdaResult LambdaResponseBody |
-  ApiGatewayResult (ApiGatewayResponse ApiGatewayResponseBody)
+data LambdaResult
+  = StandaloneLambdaResult LambdaResponseBody
+  | ApiGatewayResult (ApiGatewayResponse ApiGatewayResponseBody)
 
 -- | Options that the generated main expects
-data LambdaOptions context = LambdaOptions
-  { eventObject     :: !Lazy.ByteString
-  , functionHandler :: !String
-  , executionUuid   :: !String
-  , contextObject   :: !(Context context)
-  } deriving (Generic)
+data LambdaOptions context
+  = LambdaOptions
+      { eventObject :: !Lazy.ByteString,
+        functionHandler :: !String,
+        executionUuid :: !String,
+        contextObject :: !(Context context)
+      }
+  deriving (Generic)
