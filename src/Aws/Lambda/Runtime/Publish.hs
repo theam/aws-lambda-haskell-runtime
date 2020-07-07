143a268f961e1d6a44241a5e9cc8bcbe3fb5a37b
Nikita Tchayka <nick@booster.cloud>
2020-07-07 17:50:56 +0100

    Fix encoding

--- src/Aws/Lambda/Runtime/Publish.hs
+++ src/Aws/Lambda/Runtime/Publish.hs
@@ -1,36 +1,35 @@
-{-| Publishing of results/errors back to the
-AWS Lambda runtime API -}
+-- | Publishing of results/errors back to the
+-- AWS Lambda runtime API
 module Aws.Lambda.Runtime.Publish
-  ( result
-  , invocationError
-  , parsingError
-  , runtimeInitError
-  ) where
-
-import Control.Monad (void)
-import Data.Aeson
-import qualified Data.Text.Encoding as T
-import qualified Network.HTTP.Client as Http
+  ( result,
+    invocationError,
+    parsingError,
+    runtimeInitError,
+  )
+where
 
 import qualified Aws.Lambda.Runtime.API.Endpoints as Endpoints
 import Aws.Lambda.Runtime.Common
 import Aws.Lambda.Runtime.Context (Context (..))
 import qualified Aws.Lambda.Runtime.Error as Error
+import Control.Monad (void)
+import Data.Aeson
+import qualified Data.Text.Encoding as T
+import qualified Network.HTTP.Client as Http
 
 -- | Publishes the result back to AWS Lambda
 result :: LambdaResult -> String -> Context context -> Http.Manager -> IO ()
 result lambdaResult lambdaApi context manager = do
   let Endpoints.Endpoint endpoint = Endpoints.response lambdaApi (awsRequestId context)
   rawRequest <- Http.parseRequest endpoint
-
   let requestBody = case lambdaResult of
         (StandaloneLambdaResult res) -> Http.RequestBodyBS (T.encodeUtf8 . unLambdaResponseBody $ res)
-        (ApiGatewayResult res)       -> Http.RequestBodyLBS (encode res)
-      request = rawRequest
-                { Http.method = "POST"
-                , Http.requestBody = requestBody
-                }
-
+        (ApiGatewayResult res) -> Http.RequestBodyLBS (encode res)
+      request =
+        rawRequest
+          { Http.method = "POST",
+            Http.requestBody = requestBody
+          }
   void $ Http.httpNoBody request manager
 
 -- | Publishes an invocation error back to AWS Lambda
@@ -41,7 +40,9 @@ invocationError err lambdaApi context =
 -- | Publishes a parsing error back to AWS Lambda
 parsingError :: Error.Parsing -> String -> Context context -> Http.Manager -> IO ()
 parsingError err lambdaApi context =
-  publish err (Endpoints.invocationError lambdaApi $ awsRequestId context)
+  publish
+    err
+    (Endpoints.invocationError lambdaApi $ awsRequestId context)
     context
 
 -- | Publishes a runtime initialization error back to AWS Lambda
@@ -50,13 +51,12 @@ runtimeInitError err lambdaApi =
   publish err (Endpoints.runtimeInitError lambdaApi)
 
 publish :: ToJSON err => err -> Endpoints.Endpoint -> Context context -> Http.Manager -> IO ()
-publish err (Endpoints.Endpoint endpoint) Context{..} manager = do
+publish err (Endpoints.Endpoint endpoint) Context {..} manager = do
   rawRequest <- Http.parseRequest endpoint
-
   let requestBody = Http.RequestBodyLBS (encode err)
-      request = rawRequest
-                { Http.method = "POST"
-                , Http.requestBody = requestBody
-                }
-
+      request =
+        rawRequest
+          { Http.method = "POST",
+            Http.requestBody = requestBody
+          }
   void $ Http.httpNoBody request manager
--- src/Aws/Lambda/Utilities.hs
+++ src/Aws/Lambda/Utilities.hs
@@ -3,7 +3,7 @@ module Aws.Lambda.Utilities (toJSONText) where
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as LazyByteString
 import Data.Text
-import qualified Data.Text.Encoding as T
+import qualified Data.Text.Encoding as TE
 
 toJSONText :: ToJSON a => a -> Text
-toJSONText = T.decodeUtf8 . LazyByteString.toStrict . encode
+toJSONText = TE.decodeUtf8 . LazyByteString.toStrict . encode
