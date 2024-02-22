44f1c053ff2726cc860ecb201c39c8511b1da890
ncaq <ncaq@ncaq.net>
2024-02-22 21:38:38 +0900

    fix: update stackage resolver to lts-22.11 for build compatibility (#129)
    Avoided not being registered in stackage due to the following error.
    
    > ```
    > - aws-lambda-haskell-runtime < 0 # tried aws-lambda-haskell-runtime-4.1.2, but its *library* requires the disabled package: safe-exceptions-checked
    > ```
    >
    > <https://github.com/commercialhaskell/stackage/blob/fb2a50c986c1dab18c5aeef5bf1e12eadcbb37f3/build-constraints.yaml#L6267C1-L6267C156>
    
    The [safe-exceptions-checked](https://hackage.haskell.org/package/safe-exceptions-checked) library was deprecated,
    causing it not to build.
    It has an old list of dependencies,
    so I tried adding it to `extra-deps` and found that transformers, etc.
    require a large number of out-of-stackage-managed dependencies,
    causing it not to build.
    I could not find any other good-looking check-exception library.
    Having no choice I decided to remove the check exception mechanism itself.
    
    I believe that it should not contain any destructive changes.
    The removal of the check exception feature changes the type signature,
    but does not change the behavior,
    so it is not broken unintentionally.
    It is also sufficient for the user to remove the type signatures if they have been written.

--- src/Aws/Lambda/Runtime/Environment.hs
+++ src/Aws/Lambda/Runtime/Environment.hs
@@ -14,43 +14,43 @@ module Aws.Lambda.Runtime.Environment
 where
 
 import qualified Aws.Lambda.Runtime.Error as Error
-import Control.Exception.Safe.Checked (Throws, throw)
+import Control.Exception.Safe (throw)
 import Data.Text (Text, pack, unpack)
 import qualified System.Environment as Environment
 import qualified Text.Read as Read
 
-logGroupName :: Throws Error.EnvironmentVariableNotSet => IO Text
+logGroupName :: IO Text
 logGroupName =
   readEnvironmentVariable "AWS_LAMBDA_LOG_GROUP_NAME"
 
-logStreamName :: Throws Error.EnvironmentVariableNotSet => IO Text
+logStreamName :: IO Text
 logStreamName =
   readEnvironmentVariable "AWS_LAMBDA_LOG_STREAM_NAME"
 
-functionVersion :: Throws Error.EnvironmentVariableNotSet => IO Text
+functionVersion :: IO Text
 functionVersion =
   readEnvironmentVariable "AWS_LAMBDA_FUNCTION_VERSION"
 
-functionName :: Throws Error.EnvironmentVariableNotSet => IO Text
+functionName :: IO Text
 functionName =
   readEnvironmentVariable "AWS_LAMBDA_FUNCTION_NAME"
 
 setXRayTrace :: Text -> IO ()
 setXRayTrace = Environment.setEnv "_X_AMZN_TRACE_ID" . unpack
 
-taskRoot :: Throws Error.EnvironmentVariableNotSet => IO Text
+taskRoot :: IO Text
 taskRoot =
   readEnvironmentVariable "LAMBDA_TASK_ROOT"
 
-handlerName :: Throws Error.EnvironmentVariableNotSet => IO Text
+handlerName :: IO Text
 handlerName =
   readEnvironmentVariable "_HANDLER"
 
-apiEndpoint :: Throws Error.EnvironmentVariableNotSet => IO Text
+apiEndpoint :: IO Text
 apiEndpoint =
   readEnvironmentVariable "AWS_LAMBDA_RUNTIME_API"
 
-functionMemory :: Throws Error.Parsing => Throws Error.EnvironmentVariableNotSet => IO Int
+functionMemory :: IO Int
 functionMemory = do
   let envVar = "AWS_LAMBDA_FUNCTION_MEMORY_SIZE"
   memoryValue <- readEnvironmentVariable envVar
@@ -58,7 +58,7 @@ functionMemory = do
     Just value -> pure value
     Nothing -> throw (Error.Parsing envVar memoryValue envVar)
 
-readEnvironmentVariable :: Throws Error.EnvironmentVariableNotSet => Text -> IO Text
+readEnvironmentVariable :: Text -> IO Text
 readEnvironmentVariable envVar = do
   v <- Environment.lookupEnv (unpack envVar)
   case v of
